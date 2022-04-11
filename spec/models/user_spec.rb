b79f600291ed96048178028a999c48e8294aab8e
Jason Lutz <jason.m.lutz@gmail.com>
2022-04-11 16:46:18 -0700

    Add User auth tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,4 +5,36 @@ RSpec.describe User, type: :model do
     it { should validate_presence_of(:name) }
     it { should validate_uniqueness_of(:name) }
   end
+
+  describe 'Authentication' do
+    context 'with correct credentials' do
+      it 'returns the correct user' do
+        user = described_class.create(name: 'jason', password: 'password')
+        found = described_class.find_by(name: 'jason')&.authenticate('password')
+        expect(found).to eq(user)
+      end
+    end
+
+    context 'with incorrect password' do
+      it 'returns false' do
+        described_class.create(name: 'jason', password: 'password')
+        result = described_class.find_by(name: 'jason')&.authenticate('badPassword')
+        expect(result).to be(false)
+      end
+    end
+    context 'with incorrect name' do
+      it 'returns nil' do
+        described_class.create(name: 'jason', password: 'password')
+        result = described_class.find_by(name: 'unknown')&.authenticate('password')
+        expect(result).to be_nil
+      end
+    end
+    context 'with incorrect name and password' do
+      it 'returns nil' do
+        described_class.create(name: 'jason', password: 'password')
+        result = described_class.find_by(name: 'unknown')&.authenticate('badPassword')
+        expect(result).to be_nil
+      end
+    end
+  end
 end
