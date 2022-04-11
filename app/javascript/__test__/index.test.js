ddb11b0c2b6f08442beaf8ffcf7f56f1c9209b55
Jason Lutz <jason.m.lutz@gmail.com>
2022-04-11 12:49:54 -0700

    Add @babel/preset-env @babel/preset-react

--- app/javascript/__test__/index.test.js
+++ app/javascript/__test__/index.test.js
@@ -11,18 +11,18 @@ describe("APPLICATION ROOT", () => {
   describe("STATIC TESTS", () => {
     test("should render without crashing", () => {
       jest.spyOn(ReactDOM, "createRoot")
-      // jest.spyOn(ReactDOM, "render")
       const div = document.createElement("div");
       div.id = "root";
       document.body.appendChild(div)
       require("../index.jsx")
       expect(ReactDOM.createRoot).toHaveBeenCalledWith(div)
-      // expect(render).toHaveBeenCalledWith(
-      //   <React.StrictMode>
-      //     <App />
-      //   </React.StrictMode>
-      // )
     })
     test.todo("should call root.render")
+    // jest.spyOn(ReactDOM, "render")
+    // expect(render).toHaveBeenCalledWith(
+    //   <React.StrictMode>
+    //     <App />
+    //   </React.StrictMode>
+    // )
   })
 })
