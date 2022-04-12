6efeac2f37124ba33ec5fee5a644d40074e415ae
Jason Lutz <jason.m.lutz@gmail.com>
2022-04-12 16:22:12 -0700

    Outline test todos

--- app/javascript/components/Users/__test__/NewUserForm.test.jsx
+++ app/javascript/components/Users/__test__/NewUserForm.test.jsx
@@ -13,5 +13,40 @@ describe('NewUserForm component', () => {
       const tree = renderer.create(<NewUserForm />).toJSON();
       expect(tree).toMatchSnapshot();
     });
+
+    describe('input fields are present', () => {
+      test.todo('name input');
+      test.todo('password input');
+      test.todo('password confirmation input');
+    });
+
+    describe('register button', () => {
+      test.todo('button is present');
+      test.todo('button is initially disabled');
+    });
+
+    describe('validation display messages are absent', () => {
+      test.todo('name availability');
+      test.todo('password agreement');
+    });
+  });
+
+  describe('DYNAMIC TESTS', () => {
+    describe('password fields', () => {
+      test.todo('password fields accept input');
+      describe('password validation message', () => {
+        test.todo('non-empty non-matching passwords reveal warning message');
+        test.todo('matching passwords do not reveal warning message');
+        test.todo('warning message hidden when only password is empty');
+        test.todo('warning message hidden when only password confirmation is hidden');
+      });
+    });
+    describe('name field', () => {
+      test.todo('name field accepts input');
+      describe('name availibility checking', () => {
+        test.todo('username available message is correctly displayed');
+        test.todo('username unavailable message is correctly displayed');
+      });
+    });
   });
 });
