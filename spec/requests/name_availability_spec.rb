1232f258e65547639a6118d2d7df8022c08f9436
Jason Lutz <jason.m.lutz@gmail.com>
2022-04-13 12:46:38 -0700

    Build check_availability request spec

--- spec/requests/name_availability_spec.rb
+++ spec/requests/name_availability_spec.rb
@@ -1,11 +1,23 @@
 require 'rails_helper'
+require 'json'
 
 RSpec.describe 'Name availability checking', type: :request do
+  before do
+    User.destroy_all
+    User.create(name: 'jason', password: 'goodPassword')
+  end
+
   context 'with available user name' do
-    it 'returns name_available: true'
+    it 'returns name_available: true' do
+      get '/api/check_availability/lutz'
+      expect(JSON.parse(response.body)['name_available']).to be(true)
+    end
   end
 
   context 'with unavailable user name' do
-    it 'returns name_available: false'
+    it 'returns name_available: false' do
+      get '/api/check_availability/jason'
+      expect(JSON.parse(response.body)['name_available']).to be(false)
+    end
   end
 end
