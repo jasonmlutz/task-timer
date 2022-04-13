b500d9bbce1d9844f3156c349bb38e6101171e12
Jason Lutz <jason.m.lutz@gmail.com>
2022-04-13 12:18:12 -0700

    Continue NewUserForm build
    Design pattern is to pull out at much logic from the component
    for ease with later testing; create test todos as needed to
    allow for continued development.
    
    Note also that the three input fields are now wrapped
    in labels; this allows for easier querying of the elements
    for testing and improved accessibility.

--- app/javascript/components/Users/NewUserForm.jsx
+++ app/javascript/components/Users/NewUserForm.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 
-export async function handleRegister(credentials) {
+async function handleRegister(credentials) {
   const response = await fetch('/api/users', {
     method: 'POST',
     headers: {
@@ -24,26 +24,86 @@ export async function handleRegister(credentials) {
   }
 }
 
+function handleSubmit(credentials) {
+  if (
+    !credentials.name.length
+      || !credentials.password.length
+      || !credentials.password_confirmation.length
+  ) {
+    window.alert('please complete all fields');
+  } else if (credentials.password !== credentials.password_confirmation) {
+    window.alert('passwords do not match');
+  } else {
+    handleRegister(credentials);
+  }
+}
+
+function preventSubmit(credentials) {
+  return !credentials.name.length
+  || !credentials.password.length
+  || !(credentials.password === credentials.password_confirmation);
+}
+
+function renderPasswordStatusMessage(credentials) {
+  // determine whether we need to render any message
+  if (credentials.password.length
+    && credentials.password_confirmation.length) {
+    // set the styling and text for success or warning
+    let classes = 'border-l-4 p-2 mx-2 ';
+    let message;
+    if (credentials.password === credentials.password_confirmation) {
+      classes += 'bg-green-200 border-green-600 text-green-600';
+      message = 'Passwords match!';
+    } else {
+      classes += 'bg-yellow-200 border-yellow-600 text-yellow-600';
+      message = 'Passwords do not match!';
+    }
+    // return the element
+    return (
+      <div className="flex flex-col mb-2 mx-auto">
+        <div
+          className={classes}
+          role="alert"
+        >
+          <p>{message}</p>
+        </div>
+      </div>
+    );
+  }
+  // no message needed? great! return null
+  return null;
+}
+
+function renderRegisterButton(credentials) {
+  let classes = 'py-2 px-4 ';
+  if (preventSubmit(credentials)) {
+    // grayed-out theme, no mouse-over action
+    classes += 'bg-gray-600 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md rounded-lg cursor-not-allowed';
+  } else {
+    // colored and interactive
+    classes += 'bg-indigo-600 hover:bg-indigo-700 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 rounded-lg';
+  }
+  return (
+    <button
+      type="submit"
+      className={classes}
+      disabled={preventSubmit(credentials)}
+      onClick={(e) => {
+        e.preventDefault();
+        handleSubmit(credentials);
+      }}
+    >
+      Register
+    </button>
+  );
+}
+
 function NewUserForm() {
   const [credentials, setCredentials] = useState({
     name: '',
     password: '',
     password_confirmation: '',
   });
-
-  function handleSubmit() {
-    if (
-      !credentials.name.length
-      || !credentials.password.length
-      || !credentials.password_confirmation.length
-    ) {
-      window.alert('please complete all fields');
-    } else if (credentials.password !== credentials.password_confirmation) {
-      window.alert('passwords do not match');
-    } else {
-      handleRegister(credentials);
-    }
-  }
 
   return (
     <div className="bg-indigo-200 relative overflow-hidden h-screen">
@@ -54,33 +114,37 @@ function NewUserForm() {
           </div>
           <div className="p-6 mt-8">
             <form>
-              <input type="text" className="rounded-lg flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-indigo-600 mb-2" placeholder="Name" value={credentials.name} onChange={(e) => setCredentials({ ...credentials, name: e.target.value })} />
-              <input
-                type="password"
-                className="rounded-lg flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-indigo-600 mb-2"
-                placeholder="Password"
-                value={credentials.password}
-                onChange={(e) => setCredentials({ ...credentials, password: e.target.value })}
-              />
-              <input
-                type="password"
-                className="rounded-lg flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-indigo-600 mb-2"
-                placeholder="Confirm Password"
-                value={credentials.password_confirmation}
-                onChange={(e) => setCredentials(
-                  { ...credentials, password_confirmation: e.target.value },
-                )}
-              />
-              <button
-                type="submit"
-                className="py-2 px-4 bg-indigo-600 hover:bg-indigo-700 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 rounded-lg"
-                onClick={(e) => {
-                  e.preventDefault();
-                  handleSubmit();
-                }}
-              >
-                Register
-              </button>
+              <label htmlFor="name">
+                Name:
+                <input type="text" id="name" className="rounded-lg flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-indigo-600 mb-2" placeholder="please select a user name" value={credentials.name} onChange={(e) => setCredentials({ ...credentials, name: e.target.value })} />
+              </label>
+
+              <label htmlFor="password">
+                Password:
+                <input
+                  type="password"
+                  id="password"
+                  className="rounded-lg flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-indigo-600 mb-2"
+                  placeholder="enter a password"
+                  value={credentials.password}
+                  onChange={(e) => setCredentials({ ...credentials, password: e.target.value })}
+                />
+              </label>
+              <label htmlFor="password_confirmation">
+                Confirm Password:
+                <input
+                  type="password"
+                  id="password_confirmation"
+                  className="rounded-lg flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-indigo-600 mb-2"
+                  placeholder="please re-enter your password"
+                  value={credentials.password_confirmation}
+                  onChange={(e) => setCredentials(
+                    { ...credentials, password_confirmation: e.target.value },
+                  )}
+                />
+              </label>
+              {renderPasswordStatusMessage(credentials)}
+              {renderRegisterButton(credentials)}
             </form>
           </div>
         </div>
--- app/javascript/components/Users/__test__/NewUserForm.test.jsx
+++ app/javascript/components/Users/__test__/NewUserForm.test.jsx
@@ -1,11 +1,37 @@
+/**
+ * @jest-environment jsdom
+ */
 // REACT
 import React from 'react';
+import { createRoot } from 'react-dom/client';
 
 // TEST UTILITIES
+import { act } from 'react-dom/test-utils';
 import renderer from 'react-test-renderer';
+import { fireEvent, screen } from '@testing-library/react';
+// eslint-disable-next-line no-unused-vars
+import { toBeDisabled } from '@testing-library/jest-dom';
 
 // COMPONENTS
 import NewUserForm from '../NewUserForm';
+
+// GLOBAL SETUP
+let container;
+beforeEach(() => {
+  container = document.createElement('div');
+  document.body.appendChild(container);
+  act(() => {
+    createRoot(container).render(
+      <NewUserForm />,
+    );
+  });
+});
+
+// GLOBAL TEARDOWN
+afterEach(() => {
+  document.body.removeChild(container);
+  container = null;
+});
 
 describe('NewUserForm component', () => {
   describe('STATIC TESTS', () => {
@@ -15,38 +41,159 @@ describe('NewUserForm component', () => {
     });
 
     describe('input fields are present', () => {
-      test.todo('name input');
-      test.todo('password input');
-      test.todo('password confirmation input');
+      test('name input', () => {
+        expect(screen.getByRole('textbox', { name: /name:/i }));
+      });
+      test('password input', () => {
+        expect(screen.getByLabelText(/^password:$/i));
+      });
+      test('password confirmation input', () => {
+        expect(screen.getByLabelText(/^confirm password:$/i));
+      });
     });
 
-    describe('register button', () => {
-      test.todo('button is present');
-      test.todo('button is initially disabled');
+    describe('register button initial status', () => {
+      test('button is present', () => {
+        expect(screen.getByRole('button', { name: /register/i }));
+      });
+      test('button is disabled', () => {
+        expect(screen.getByRole('button', { name: /register/i })).toBeDisabled();
+      });
+      test('button color is gray', () => {
+        const button = screen.getByRole('button', { name: /register/i });
+        expect(button.classList).toContain('bg-gray-600');
+        expect(button.classList).not.toContain('bg-indigo-600');
+      });
+      test('cursor is not-available', () => {
+        const button = screen.getByRole('button', { name: /register/i });
+        expect(button.classList).toContain('cursor-not-allowed');
+      });
     });
 
     describe('validation display messages are absent', () => {
-      test.todo('name availability');
-      test.todo('password agreement');
+      test('name availability', () => {
+        expect(screen.queryAllByText(/not available/)).toStrictEqual([]);
+      });
+      test('password agreement', () => {
+        expect(screen.queryAllByText(/do not match/)).toStrictEqual([]);
+      });
     });
   });
 
   describe('DYNAMIC TESTS', () => {
+    // LOCAL SETUP
+    let nameField;
+    let passwordField;
+    let passwordConfirmField;
+    beforeEach(() => {
+      nameField = screen.getByRole('textbox', { name: /name:/i });
+      passwordField = screen.getByLabelText(/^password:$/i);
+      passwordConfirmField = screen.getByLabelText(/^confirm password:$/i);
+    });
+    describe('register button with valid field values', () => {
+      let button;
+      beforeEach(() => {
+        button = screen.getByRole('button', { name: /register/i });
+        act(() => {
+          fireEvent.change(nameField, { target: { value: 'validName' } });
+          fireEvent.change(passwordField, { target: { value: 'goodPassword' } });
+          fireEvent.change(passwordConfirmField, {
+            target: { value: 'goodPassword' },
+          });
+        });
+      });
+      test('button is enabled', () => {
+        expect(button).toBeEnabled();
+      });
+      test('button has cursor-allowed', () => {
+        expect(button.classList).not.toContain('cursor-not-allowed');
+      });
+      test('button is colored', () => {
+        expect(button.classList).toContain('bg-indigo-600');
+      });
+      test('button has hover effects', () => {
+        expect(button.classList).toContain('hover:bg-indigo-700');
+      });
+    });
     describe('password fields', () => {
-      test.todo('password fields accept input');
-      describe('password validation message', () => {
-        test.todo('non-empty non-matching passwords reveal warning message');
-        test.todo('matching passwords do not reveal warning message');
-        test.todo('warning message hidden when only password is empty');
-        test.todo('warning message hidden when only password confirmation is hidden');
+      test('password fields accept input', () => {
+        act(() => {
+          fireEvent.change(passwordField, { target: { value: 'firstPassword' } });
+          fireEvent.change(passwordConfirmField, {
+            target: { value: 'secondPassword' },
+          });
+        });
+        expect(passwordField.value).toBe('firstPassword');
+        expect(passwordConfirmField.value).toBe('secondPassword');
+      });
+      describe('password validation message & warning', () => {
+        test('non-empty non-matching passwords reveal warning', () => {
+          act(() => {
+            fireEvent.change(passwordField, { target: { value: 'password' } });
+            fireEvent.change(passwordConfirmField, {
+              target: { value: 'badPassword' },
+            });
+          });
+          expect(screen.queryByText(/passwords do not match/i)).not.toBeNull();
+        });
+        test('matching passwords reveal matching message', () => {
+          act(() => {
+            fireEvent.change(passwordField, { target: { value: 'password' } });
+            fireEvent.change(passwordConfirmField, {
+              target: { value: 'password' },
+            });
+          });
+          expect(screen.queryByText(/passwords match/i)).not.toBeNull();
+        });
+        test('neigher message nor warning shown when only password is empty', () => {
+          act(() => {
+            fireEvent.change(passwordConfirmField, {
+              target: { value: 'password' },
+            });
+          });
+          expect(screen.queryByText(/passwords do not match/i)).toBeNull();
+          expect(screen.queryByText(/passwords match/i)).toBeNull();
+        });
+        test('neigher message nor warning shown when only password confirmation is hidden', () => {
+          act(() => {
+            fireEvent.change(passwordConfirmField, {
+              target: { value: 'password' },
+            });
+          });
+          expect(screen.queryByText(/passwords do not match/i)).toBeNull();
+          expect(screen.queryByText(/passwords match/i)).toBeNull();
+        });
       });
     });
     describe('name field', () => {
-      test.todo('name field accepts input');
+      test('name field accepts input', () => {
+        act(() => {
+          fireEvent.change(nameField, { target: { value: 'validName' } });
+        });
+        expect(nameField.value).toBe('validName');
+      });
       describe('name availibility checking', () => {
         test.todo('username available message is correctly displayed');
         test.todo('username unavailable message is correctly displayed');
       });
+    });
+    describe('submit button action', () => {
+      test.todo('triggers handleSubmit call');
+      test.todo('triggers handleRegister call');
+    });
+  });
+
+  describe('SUPPORTING RESOURCES TESTS', () => {
+    describe('function handleRegister', () => {
+      test.todo('has no return');
+      test.todo('triggers a fetch API call');
+      test.todo('triggers alert if !response.ok');
+      test.todo('triggers alert if user.error');
+      test.todo('logs user object on successful fetch');
+    });
+
+    describe('function allowSubmit', () => {
+      test.todo('');
     });
   });
 });
